75aaf9a5cb125bdedc346c5cdcc983c6cdf540d3
IacobIonut01 <paulionut2003@gmail.com>
2022-04-01 17:38:27 +0000

    SystemUI: Redesign QS Tiles to A11 style
    alibei:
    - Remove blur support
    - Make all tiles round
    
    - Brings up the A11 style of tiles
      + Added morph status of tiles (Circle - Active | Rounded Rect - Inactive/Disabled)
      + Kept compatibility with the secondaryLabel from A12
    - Initial support for columns/rows control in QQS, QS and Customizer
    - Removed color differencce between QSPanel and Notification Drawer
    - Aligned Notification Footer buttons with the notifications (History | Clear All)
    - Added transparency for blur support
    [Temporary removed] - Keep showing notifications when the QSPanel is fully expanded
    
    Bugs / W.I.P.....
    [Temporary removed] - After exiting the QS Customizer, the Notification Drawer's scrim color goes to TRANSPARENT until the user scroll slightly
    
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    
    Change-Id: I9630d414cd6db3b896dcc6f0591b1cd728012f36
    Signed-off-by: alibei <sebastiankotzias83@gmail.com>
    Signed-off-by: sushmit1 <sushmit718@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -4359,6 +4359,9 @@ public class NotificationPanelViewController extends PanelViewController {
                 mView.setAccessibilityPaneTitle(determineAccessibilityPaneTitle());
             }
             mNotificationStackScrollLayoutController.setMaxTopPadding(mQsMaxExpansionHeight);
+            float qsExpansionFraction = computeQsExpansionFraction();
+            int qsPanelBottomY = calculateQsBottomPosition(qsExpansionFraction);
+            mScrimController.setQsPosition(qsExpansionFraction, qsPanelBottomY);
         }
     }
 
