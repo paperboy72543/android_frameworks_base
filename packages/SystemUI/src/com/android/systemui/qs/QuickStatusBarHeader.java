129a3150c00109238df6d156e80eedc5c0384288
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-26 23:43:56 +0530

    SystemUI: Fix QS clock overlapping on UI mode change
    Calling updateAnimators() in a separate runnable seems to
    cause QSB header elements to overlap each other when switching
    between dark and light mode. This change was introduced in
    0558e1211604b3b6f465a394832bbf8fa06d1dfe and seems unnecessary so lets revert it.
    
    Change-Id: I14e60b608f2aac373e71fec0a6ab7d8e787d2457
    Signed-off-by: sushmit1 <sushmit718@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -185,7 +185,7 @@ public class QuickStatusBarHeader extends FrameLayout {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         if (mDatePrivacyView.getMeasuredHeight() != mTopViewMeasureHeight) {
             mTopViewMeasureHeight = mDatePrivacyView.getMeasuredHeight();
-            post(this::updateAnimators);
+            updateAnimators();
         }
     }
 
